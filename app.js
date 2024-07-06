025e765f5e8672230629b99b20c2ba4bd78e2bf5
AbdulMoiz-Ali <abdulmoizali321@gmail.com>
2024-07-06 16:13:52 +0500

    event

---
+++ app.js
@@ -0,0 +1,65 @@
+document.write("<br>");
+document.write("<h3>Question no 01</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques1() ">Click Me</button>');
+document.write("<br>");
+document.write("<br>");
+function ques1() {
+
+}
+
+// question no 2
+
+document.write("<h3>Question no 02</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques2();">Click Me</button>');
+function ques2() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 3
+
+document.write("<h3>Question no 03</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques3()">Click Me</button>');
+function ques3() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 4
+
+document.write("<h3>Question no 04</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques4()">Click Me</button>');
+function ques4() {
+
+};
+document.write("<br>");
+document.write("<br>");
+//question no 5
+document.write("<h3>Question no 05</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques5()">Click Me</button>');
+function ques5() {
+
+}
+
+// document.write('<div id="pere"></div>')
+document.write("<br>");
+document.write("<br>");
+
+document.write('<div style="color: blue;background-color: black;"><br><h1>Best OF luck<span style="font-size:50px;">👋</span></h1><br></div>')//&#128075
