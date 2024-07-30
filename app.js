330a59d45cb7171b7e684b0c8f31d25ebb6e6739
AbdulMoiz-Ali <170756984+AbdulMoiz-Ali@users.noreply.github.com>
2024-07-30 19:48:39 +0500

    question solve

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ document.write("<h3>Question no 01</h3>");
 document.write("<br>");
 document.write("<h4>Result :</h4>");
 document.write("<br>");
-document.write('<button id="btn" onclick="ques1() ">Click Me</button>');
+document.write('<button id="btn" onclick="ques1();">Click Me</button>');
 document.write("<br>");
 document.write("<br>");
 function ques1() {
-
+alert("Hello Would!")
 }
 
 // question no 2
@@ -55,6 +55,58 @@ document.write("<h4>Result :</h4>");
 document.write("<br>");
 document.write('<button id="btn" onclick="ques5()">Click Me</button>');
 function ques5() {
+
+}
+
+// document.write('<div id="pere"></div>')
+document.write("<br>");
+document.write("<br>");
+document.write(`<div class="moiz">
+      <h1>Assignment</h1>
+      <p class="ali">
+        <object data="./nHrPoN_.pdf" type="application/pdf" width="100%" height="500px">
+          <p>
+            Unable to display PDF file.
+            <a href="./nHrPoN_.pdf">Download</a>
+            instead.
+          </p>
+        </object>
+      </p>
+    </div>`)
+document.write("<br>");
+
+//question no 1
+
+document.write("<h3>Question no 01</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques31()">Click Me</button>');
+function ques31() {
+
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 2
+
+document.write("<h3>Question no 02</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques42()">Click Me</button>');
+function ques42() {
+
+};
+document.write("<br>");
+document.write("<br>");
+//question no 3
+document.write("<h3>Question no 03</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques53()">Click Me</button>');
+function ques53() {
 
 }
 
